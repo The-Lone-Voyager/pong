3f9b6425e84ae5b151f06f5c142e41605b594383
Dakota.Barron <Dakota.Barron@S52209>
2016-10-15 10:46:18 -0500

    barebones, clean. should be final commit for just initializing the screen layout.

--- GameObjects/GameObject.cs
+++ GameObjects/GameObject.cs
@@ -29,44 +29,6 @@ namespace pong
             MakeTextInvisible();
         }
 
-        public void MoveUp()    // moves the object character up one unit
-        {
-            GoToPositionOnScreen();     // go to where the object character currently is
-
-            MakeTextInvisible();        // set text color to same as background so it is not visible
-
-            Write(" ");                 // output a space to get rid of current object character
-
-            --Top;                      // move cursor position marker up one  
-
-            GoToPositionOnScreen();                           // move cursor to new position, which is up one row
-
-            MakeTextVisible();                              // set text color to visible
-
-            Draw();                                         // output object character in new position
-
-            MakeTextInvisible();                            // make any new text invisible again
-        }
-
-        public void MoveDown()                              // moves the object character down one unit
-        {
-            GoToPositionOnScreen();                           // go to where the object character currently is
-
-            MakeTextInvisible();                            // set text color to same as background so it is not visible
-
-            Write(" ");                                     // output a space to get rid of current object character
-
-            ++Top;                                          // move cursor position marker down one  
-
-            GoToPositionOnScreen();                           // move cursor to new position, which is down one row
-
-            MakeTextVisible();                              // set text color to visible
-
-            Draw();                                         // output object character in new position
-
-            MakeTextInvisible();                            // make any new text invisible again
-        }
-
         private int Top { get; set; }   // the current position of the object character from the top of the console window
                                                                       
         private int Left { get; set; }  // the current position of the object character from the left of the console window
